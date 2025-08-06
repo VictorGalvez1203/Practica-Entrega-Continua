15b71d06828da8537b5e958f6e917b8ad5c409ef
Victor Galvez <victorgalvez0312@gmail.com>
2025-08-06 01:27:49 -0400

    fix: nuevos cambios para RENDER

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3008;
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo desde Express ITLA, por victor Galvez/2023-1836');
+    res.send('Hola Mundo desde Express ITLA, por victor Galvez/2023-1836 desplegado en Render');
 });
 
 app.listen(PORT, () => {
